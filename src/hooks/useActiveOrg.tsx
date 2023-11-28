b1705dcefd77bc45821315d63bb2caf8f0e6bb96
Itai Levi <italev46@gmail.com>
2023-11-28 10:07:30 -0500

    refactor(useActiveOrg): local storage is timestamp instead of actual value

--- src/hooks/useActiveOrg.tsx
+++ src/hooks/useActiveOrg.tsx
@@ -2,33 +2,33 @@ import { getActiveOrgId, setActiveOrgId as setActiveOrgIdCookie } from "@propela
 import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../AuthContext"
 
-const ORG_SELECTION_LOCAL_STORAGE_KEY = "__last_selected_org"
+const DEPRECATED_ORG_SELECTION_LOCAL_STORAGE_KEY = "__last_selected_org"
 const ACTIVE_ORG_ID_LOCAL_STORAGE_KEY = "__ACTIVE_ORG_ID"
 
-export function useActiveOrg() {
+interface ActiveOrg {
+    activeOrgId?: string
+    setActiveOrgId: (orgId: string) => void
+}
+
+export function useActiveOrg(): ActiveOrg | undefined {
     const context = useContext(AuthContext)
     if (context === undefined) {
         throw new Error("useActiveOrg must be used within an AuthProvider or RequiredAuthProvider")
     }
+
     const [activeOrgIdState, setActiveOrgIdState] = useState<string | undefined>(getActiveOrgId())
-
-    const handleLocalStorageChange = () => {
-        setActiveOrgIdState(localStorage.getItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY) ?? undefined)
-    }
-
-    const setActiveOrgId = (orgId: string) => {
-        setActiveOrgIdCookie(orgId)
-        setActiveOrgIdState(orgId)
-        localStorage.setItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY, orgId)
-    }
 
     // If a cookie exists for the active org on first load, set it in local storage.
     useEffect(() => {
         const isLocalStorageNotSet = activeOrgIdState && !localStorage.getItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY)
         if (isLocalStorageNotSet) {
-            localStorage.setItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY, activeOrgIdState)
+            localStorage.setItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY, Date.now().toString())
         }
     }, [activeOrgIdState])
+
+    const handleLocalStorageChange = () => {
+        setActiveOrgIdState(getActiveOrgId())
+    }
 
     useEffect(() => {
         window.addEventListener("storage", handleLocalStorageChange)
@@ -38,24 +38,38 @@ export function useActiveOrg() {
         }
     }, [])
 
+    const { loading, authInfo } = context
+    if (loading) {
+        return undefined
+    }
+    if (!authInfo) {
+        return undefined
+    }
+
+    const setActiveOrgId = (orgId: string) => {
+        setActiveOrgIdCookie(orgId)
+        setActiveOrgIdState(orgId)
+        localStorage.setItem(ACTIVE_ORG_ID_LOCAL_STORAGE_KEY, Date.now().toString())
+    }
+
     return { activeOrgId: activeOrgIdState, setActiveOrgId }
 }
 
 /**
- * @deprecated Use useActiveOrg instead.
+ * @deprecated Use `useActiveOrg` instead.
  */
 export function saveOrgSelectionToLocalStorage(orgIdOrName: string) {
     if (localStorage) {
-        localStorage.setItem(ORG_SELECTION_LOCAL_STORAGE_KEY, orgIdOrName)
+        localStorage.setItem(DEPRECATED_ORG_SELECTION_LOCAL_STORAGE_KEY, orgIdOrName)
     }
 }
 
 /**
- * @deprecated Use useActiveOrg instead.
+ * @deprecated Use `useActiveOrg` instead.
  */
 export function loadOrgSelectionFromLocalStorage(): string | null {
     if (localStorage) {
-        return localStorage.getItem(ORG_SELECTION_LOCAL_STORAGE_KEY)
+        return localStorage.getItem(DEPRECATED_ORG_SELECTION_LOCAL_STORAGE_KEY)
     }
     return null
 }
